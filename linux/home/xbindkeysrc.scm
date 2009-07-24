d97b9bbc8e38c68d54d83e37a43ef2a9c90c2de2
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-07-24 14:31:57 +1000

    added x input binding to emulate horizontal scrolling for SHIFT+scroll

---
+++ linux/home/xbindkeysrc.scm
@@ -0,0 +1,7 @@
+; NOTE: requires:
+;    apt-get install xbindkeys xautomation
+
+; bind shift + vertical scroll to horizontal scroll events
+(xbindkey '(shift "b:4") "xte 'mouseclick 6'")
+(xbindkey '(shift "b:5") "xte 'mouseclick 7'")
+
