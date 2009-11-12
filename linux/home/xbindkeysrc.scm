10cbe36d8916f1016b30c6b6ee08970804378352
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-11-12 21:53:14 +1100

    x key stuff

--- linux/home/xbindkeysrc.scm
+++ linux/home/xbindkeysrc.scm
@@ -5,7 +5,10 @@
 (xbindkey '(shift "b:4") "xte 'mouseclick 6'")
 (xbindkey '(shift "b:5") "xte 'mouseclick 7'")
 
+; f13 = play/pause
 (xbindkey '("c:191") "songbird --pause")
+
+; meta+alt+left/right for prev/next ( not yet working...)
 (xbindkey '("m:0x48" "c:64" left) "songbird --previous")
-(xbindkey '("m:0x48" "c:64" right) "songbird --next")
+(xbindkey '("m:0x48" "c:64" "c:0x") "songbird --next")
 
