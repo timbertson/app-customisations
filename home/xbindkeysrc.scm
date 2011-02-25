47676e53870de0236b4c30fe18b5e829173dd9ac
gfxmonk <tim@gfxmonk.net>
2011-02-25 22:38:04 +1100

    .

--- home/xbindkeysrc.scm
+++ home/xbindkeysrc.scm
@@ -36,19 +36,5 @@
 ; kill windows with meta+shift+middle-click
 (xbindkey '(alt mod4 "b:2") "xte 'keyup Alt_L' 'keydown Shift_L' 'key c' 'keyup Shift_L' 'keydown Alt_L'")
 
-; ctrl+h for backspace
-; (xbindkey '(control "c:43") "xvkbd -xsendevent -text '\\b'")
-
 ; ctrl+m for enter
 (xbindkey '(control "c:58") "xvkbd -xsendevent -text '\\r'")
-
-; f13 = play/pause
-;(xbindkey '("c:191") "songbird --pause")
-; f14 & f15 for prev/next
-;(xbindkey '("c:193") "songbird --next")
-;(xbindkey '("c:192") "songbird --previous")
-
-; meta+alt+left/right for prev/next ( not yet working...)
-;(xbindkey '("m:0x48" "c:64" left) "songbird --previous")
-;(xbindkey '("m:0x48" "c:64" "c:0x") "songbird --next")
-
