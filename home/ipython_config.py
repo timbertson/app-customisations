27f762daf3484382a93a1f90ae6d3dd7afda6931
gfxmonk <tim@gfxmonk.net>
2012-04-22 15:56:24 +1000

    .

---
+++ home/ipython_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+c.TerminalInteractiveShell.confirm_exit = False
+c.PromptManager.in_template  = '>>> '
+c.PromptManager.out_template = ' => '
+c.PromptManager.justify = False
+c.PromptManager.in2_template = '... '
