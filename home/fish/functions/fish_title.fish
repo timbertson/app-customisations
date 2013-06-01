9c17b11c255f1b9fe0f2b2106bd867a2d290cecf
Tim Cuthbertson <tim@gfxmonk.net>
2013-06-01 15:32:28 +1000

    .

--- home/fish/functions/fish_title.fish
+++ home/fish/functions/fish_title.fish
@@ -1,11 +1,11 @@
 function fish_title
     if [ $_ = 'fish' ]
+        printf '#'
         if [ $PWD = $HOME ]
           printf '~'
         else
           printf (basename $PWD)' ('(dirname (prompt_pwd))')'
         end
-        printf '$'
     else
         printf $_' ('(prompt_pwd)')'
     end
