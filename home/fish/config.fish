22342af7a5ae68bd9424f58211d828c7dec4456d
Tim Cuthbertson <tim@gfxmonk.net>
2013-06-18 19:38:28 +1000

    .

--- home/fish/config.fish
+++ home/fish/config.fish
@@ -1,13 +1,18 @@
+set NODE_ROOT /usr/lib/nodejs
+if [ -d /usr/local/share/npm ]
+	set NODE_ROOT /usr/local/share/npm
+end
+
 set -x PATH \
 	~/.bin/overrides \
 	$PATH \
 	/sbin/ \
 	~/.cabal/bin \
-	/usr/lib/nodejs/bin \
+	$NODE_ROOT/bin \
 	~/bin \
 	~/.bin
 
-set NODE_PATH $NODE_PATH /usr/lib/nodejs/lib/node_modules
+set NODE_PATH $NODE_PATH $NODE_ROOT/lib/node_modules
 set FISH_CLIPBOARD_CMD "cat" # Stop that.
 set BROWSER firefox
 set -x EDITOR vim
