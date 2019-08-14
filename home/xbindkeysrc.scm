d312e72f5502fa0e41512b0d1a2896f2eed6f000
Tim Cuthbertson <tim@gfxmonk.net>
2019-08-14 12:30:58 +1000

    .

--- home/xbindkeysrc.scm
+++ home/xbindkeysrc.scm
@@ -14,6 +14,11 @@
 ; alt + scroll to switch tabs
 (xbindkey '(alt "b:4") "xte 'keyup Alt_L' 'keydown Control_L' 'key Page_Up' 'keyup Control_L' 'keydown Alt_L'")
 (xbindkey '(alt "b:5") "xte 'keyup Alt_L' 'keydown Control_L' 'key Page_Down' 'keyup Control_L' 'keydown Alt_L'")
+
+(xbindkey '(shift "b:4") "xte 'keyup Shift_L' 'keydown Control_L' 'key Page_Up' 'keyup Control_L' 'keydown Shift_L'")
+(xbindkey '(shift "b:5") "xte 'keyup Shift_L' 'keydown Control_L' 'key Page_Down' 'keyup Control_L' 'keydown Shift_L'")
+
+
 ; alt + meta + scroll to switch workspaces
 (xbindkey '(mod4 "b:4") "xte 'key Page_Up'")
 (xbindkey '(mod4 "b:5") "xte 'key Page_Down'")
