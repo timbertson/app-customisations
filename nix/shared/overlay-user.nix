4a322807f65e1244058cb4f87d6fdfb3777b0004
Tim Cuthbertson <tim@gxmonk.net>
2020-09-14 14:15:26 +1000

    fix OSX build

--- nix/shared/overlay-user.nix
+++ nix/shared/overlay-user.nix
@@ -4,4 +4,5 @@ with super; {
 	python3Packages = super.python3Packages // {
 		python-language-server = super.python3Packages.python-language-server.override { providers = []; };
 	};
+	sbt = super.sbt.override { jre = super.openjdk11; };
 }
