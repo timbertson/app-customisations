2f3118b0a2898613810f8c0a2489fed563ddb319
Tim Cuthbertson <tim@gxmonk.net>
2022-01-21 22:06:13 +1100

    .

--- nix/shared/overlay-user.nix
+++ nix/shared/overlay-user.nix
@@ -1,8 +1,15 @@
 self: super:
 with super; {
-	# disable fancy language server rubbish
 	python3Packages = super.python3Packages // {
-		python-language-server = super.python3Packages.python-language-server.override { providers = []; };
+		json-schema-for-humans = pkgs.poetry2nix.mkPoetryApplication rec {
+			src = super.fetchFromGitHub {
+				owner = "coveooss";
+				repo = "json-schema-for-humans";
+				rev = "v0.39.1";
+				sha256 = "0pwzc0gaf2w8a03qx56v79dzmlw032rx16im44dw0ln88xfgi016";
+			};
+			projectDir = src;
+		};
 	};
 	sbt = super.sbt.override { jre = super.openjdk11; };
 	scala = super.scala.override { jre = super.openjdk11; };
