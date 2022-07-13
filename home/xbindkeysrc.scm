a9434ab6483b3444c427c0bf13bcd976024a9b2c
Tim Cuthbertson <tim@gfxmonk.net>
2022-07-13 21:03:14 +1000

    .

--- home/xbindkeysrc.scm
+++ home/xbindkeysrc.scm
@@ -5,6 +5,10 @@
 (xbindkey '(shift "b:4") "xte 'keyup Shift_L' 'keydown Control_L' 'key Page_Up' 'keyup Control_L' 'keydown Shift_L'")
 (xbindkey '(shift "b:5") "xte 'keyup Shift_L' 'keydown Control_L' 'key Page_Down' 'keyup Control_L' 'keydown Shift_L'")
 
+; use mac-style tab swiching
+(xbindkey '(control shift bracketleft ) "xte 'keyup bracketleft'  'keyup Shift_L' 'key Page_Up'   'keydown Shift_L'")
+(xbindkey '(control shift bracketright) "xte 'keyup bracketright' 'keyup Shift_L' 'key Page_Down' 'keydown Shift_L'")
+
 
 ; alt + meta + scroll to switch workspaces
 (xbindkey '(mod4 "b:4") "xte 'key Page_Up'")
