0fde2f477adfba31f5f81642c3a0089a94deb007
Tim Cuthbertson <tim@gxmonk.net>
2022-12-07 16:34:42 +1100

    .

--- nix/host-tcuthbertson.nix
+++ nix/host-tcuthbertson.nix
@@ -12,6 +12,11 @@ in
 	sources = super.sources // {
 		fetlock = self.localHead ../../rust/fetlock;
 	};
+
+	# temporary gup 0.8.1
+	gup = let src = self.localHead ../../timbertson/gup; in
+		(super.callPackage "${src}/nix/gup-python.nix" {}); #.overrideAttrs (orig: { src; });
+
 	installedPackages = super.installedPackages ++ (with self; [
 		gnupg
 		vendir
--- nix/shared/overlay-user.nix
+++ nix/shared/overlay-user.nix
@@ -1,16 +1,16 @@
 self: super:
 with super; {
-	python3Packages = super.python3Packages // {
-		json-schema-for-humans = pkgs.poetry2nix.mkPoetryApplication rec {
-			src = super.fetchFromGitHub {
-				owner = "coveooss";
-				repo = "json-schema-for-humans";
-				rev = "v0.39.1";
-				sha256 = "0pwzc0gaf2w8a03qx56v79dzmlw032rx16im44dw0ln88xfgi016";
-			};
-			projectDir = src;
-		};
-	};
+	# python3Packages = super.python3Packages // {
+	# 	json-schema-for-humans = pkgs.poetry2nix.mkPoetryApplication rec {
+	# 		src = super.fetchFromGitHub {
+	# 			owner = "coveooss";
+	# 			repo = "json-schema-for-humans";
+	# 			rev = "v0.39.1";
+	# 			sha256 = "0pwzc0gaf2w8a03qx56v79dzmlw032rx16im44dw0ln88xfgi016";
+	# 		};
+	# 		projectDir = src;
+	# 	};
+	# };
 	sbt = super.sbt.override { jre = super.openjdk11; };
 	scala = super.scala.override { jre = super.openjdk11; };
 }
